d43da661bc516c4bcdaa49bf332cb93f7f8a4bbe
kamilcodes <kc.chrzanowski@gmail.com>
2021-07-16 21:07:31 +0200

    add functions to play btn to generate and display a random hand for each player

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 let btnPlay = document.getElementById("btn-play");
+let player1HandBox = document.getElementById("player1-hand-box");
+let player2HandBox = document.getElementById("player2-hand-box");
+let player1Hand = document.getElementById("player1-hand");
+let player2Hand = document.getElementById("player2-hand");
+
 let hands = ["rock", "paper", "scissors"];
+let currentHandPlayer1;
+let currentHandPlayer2;
 
 function play() {
-  getRandomHand();
+  getRandomHandPlayer1();
+  getRandomHandPlayer2();
 }
 
-function getRandomHand() {
+function getRandomHandPlayer1() {
   let randomNumber = Math.floor(Math.random() * hands.length);
   let hand = hands[randomNumber];
-  //   console.log(hand);
+  currentHandPlayer1 = hand;
+  if (currentHandPlayer1 === "rock") {
+    player1Hand.src = "assets/hand-rock-regular.svg";
+  } else if (currentHandPlayer1 === "paper") {
+    player1Hand.src = "assets/hand-paper-regular.svg";
+  } else if (currentHandPlayer1 === "scissors") {
+    player1Hand.src = "assets/hand-scissors-regular.svg";
+  }
+}
+function getRandomHandPlayer2() {
+  let randomNumber = Math.floor(Math.random() * hands.length);
+  let hand = hands[randomNumber];
+  currentHandPlayer2 = hand;
+  if (currentHandPlayer1 === "rock") {
+    player2Hand.src = "assets/hand-rock-regular.svg";
+  } else if (currentHandPlayer2 === "paper") {
+    player2Hand.src = "assets/hand-paper-regular.svg";
+  } else if (currentHandPlayer2 === "scissors") {
+    player2Hand.src = "assets/hand-scissors-regular.svg";
+  }
 }
